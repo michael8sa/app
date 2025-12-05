161b55c5be580bf8d9f26de3ea92355dd4c5ee23
Michael <michael8s@icloud.com>
2025-12-05 10:35:38 +0200

    Delete sql-queries directory

--- sql-queries/aggregate.sql
+++
@@ -1,8 +0,0 @@
----Aggregate functions--- AVG(), COUNT, MIN(), MAX(), SUM()
-
-SELECT COUNT(emp_no) FROM "employees"
-SELECT * FROM public.salaries             
-SELECT MAX(salary) FROM public.salaries
-SELECT emp_no FROM public.salaries WHERE salary = 158220
-SELECT SUM(salary) FROM public.salaries
-SELECT first_name, last_name FROM public.employees WHERE "first_name" = 'Mayumi' AND "last_name" = 'Schueller';
--- sql-queries/orderby.sql
+++
@@ -1,9 +0,0 @@
----ORDER BY---
-SELECT * FROM employees
-ORDER BY first_name ASC, last_name DESC;
-
-SELECT * FROM employees
-ORDER BY birth_date DESC, hire_date DESC;
-
-SELECT * FROM employees;
-ORDER BY LENGTH(first_name) DESC, LENGTH(last_name) DESC;
